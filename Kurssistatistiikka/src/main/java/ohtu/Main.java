45bf8a567c020d96cd63a673d381baed8fb1e6d2
minna <32199029+mshroom@users.noreply.github.com>
2018-11-13 20:48:25 +0200

    week 3, exercise 1 fixed printing

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -17,16 +17,27 @@ public class Main {
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
 
+        /*
         System.out.println("json-muotoinen data:");
         System.out.println(bodyText);
+        */
 
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        int sumE = 0;
+        int sumH = 0;
+        for (int i = 0; i <= subs.length - 1; i ++) {
+            Submission s = subs[i];
+            sumE += s.getExercises().length;
+            sumH += s.getHours();
+        }
 
-        System.out.println("Oliot:");
+        System.out.println("Opiskelijanumero " + studentNr);
+        System.out.println("");
         for (Submission submission : subs) {
             System.out.println(submission);
         }
-
+        System.out.println("");
+        System.out.println("Yhteensä: " + sumE + " tehtävää " + sumH + " tuntia");
     }
 }
