c77419239eb878c1fed51ceecbc18dda39b2bc7e
minna <32199029+mshroom@users.noreply.github.com>
2018-11-14 19:10:35 +0200

    week 3, exercise 8

--- LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -6,11 +6,15 @@ import ohtu.domain.User;
 import java.util.ArrayList;
 import java.util.List;
 import ohtu.data_access.UserDao;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Component
 public class AuthenticationService {
 
     private UserDao userDao;
 
+    @Autowired
     public AuthenticationService(UserDao userDao) {
         this.userDao = userDao;
     }
